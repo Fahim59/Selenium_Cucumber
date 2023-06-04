f746a1c7feada804d789db8e0af10fe86893b95d
fahim59 <mrahaman59@yahoo.com>
2023-06-04 16:04:41 +0600

    TESTNG Annotation added

--- src/test/java/Real_Scenario/TestNGRunnerTest.java
+++ src/test/java/Real_Scenario/TestNGRunnerTest.java
@@ -1,7 +1,20 @@
-//package Real_Scenario;
-//
-//import io.cucumber.testng.CucumberOptions;
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//
-//@CucumberOptions(plugin = "message:target/cucumber-report.ndjson")
-//public class TestNGRunnerTest extends AbstractTestNGCucumberTests { }
+package Real_Scenario;
+
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+@CucumberOptions(plugin = "message:target/cucumber-report.ndjson")
+public class TestNGRunnerTest extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("\nBEFORE CLASS\n");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("\nAFTER CLASS\n");
+    }
+}
