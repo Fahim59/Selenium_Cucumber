a14404d70dceb2a91dd5ba24abe26d01973c0468
fahim59 <mrahaman59@yahoo.com>
2023-06-04 15:23:47 +0600

    run with JUnit Runner

---
+++ src/test/java/Real_Scenario/JUnitRunnerTest.java
@@ -0,0 +1,9 @@
+package Real_Scenario;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions()
+public class JUnitRunnerTest { }
