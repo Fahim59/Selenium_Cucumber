686feac280065e42df0967a0fdad73e99941f754
fahim59 <mrahaman59@yahoo.com>
2023-06-04 15:58:34 +0600

    JUnit Annotation added

--- src/test/java/Real_Scenario/JUnitRunnerTest.java
+++ src/test/java/Real_Scenario/JUnitRunnerTest.java
@@ -2,8 +2,21 @@ package Real_Scenario;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions()
-public class JUnitRunnerTest { }
+public class JUnitRunnerTest {
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("\nBEFORE CLASS\n");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("\nAFTER CLASS\n");
+    }
+}
--- src/test/java/Real_Scenario/TestNGRunnerTest.java
+++ src/test/java/Real_Scenario/TestNGRunnerTest.java
@@ -1,7 +1,7 @@
-package Real_Scenario;
-
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(plugin = "message:target/cucumber-report.ndjson")
-public class TestNGRunnerTest extends AbstractTestNGCucumberTests { }
+//package Real_Scenario;
+//
+//import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//
+//@CucumberOptions(plugin = "message:target/cucumber-report.ndjson")
+//public class TestNGRunnerTest extends AbstractTestNGCucumberTests { }
